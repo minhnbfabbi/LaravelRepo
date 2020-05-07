809db5ed5cbcfcfc6aea09ac8cda1ff96b622f1b
minhnb <minhnb@fabbi.io>
2020-05-07 13:37:28 +0700

    commit pre commit hook

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        \App\Console\Commands\PrecommitHook::class,
+        \App\Console\Commands\InstallHooks::class,
     ];
 
     /**
